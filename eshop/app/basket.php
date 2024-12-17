925878b5da2c60a66e287fa5cf6f759b75c71f3f
angelicxx <ZhurbenkoNV22@st.ithub.ru>
2024-12-18 00:29:53 +0300

    Добавлена папка eshop

---
+++ eshop/app/basket.php
@@ -0,0 +1,20 @@
+<p>Вернуться в <a href='/catalog'>каталог</a></p>
+<h1>Ваша корзина</h1>
+<table>
+<tr>
+	<th>N п/п</th>
+	<th>Название</th>
+	<th>Автор</th>
+	<th>Год издания</th>
+	<th>Цена, руб.</th>
+	<th>Количество</th>
+	<th>Удалить</th>
+</tr>
+</table>
+
+<p>Всего товаров в корзине на сумму:   руб.</p>
+
+<div style="text-align:center">
+	<input type="button" value="Оформить заказ!"
+                      onclick="location.href='/create_order'" />
+</div>
